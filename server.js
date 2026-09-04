52babffe54328262a3149463448d0f5b45adfc52
abdul-haseeb-khokhar <h.khokhar2015@gmail.com>
2026-09-04 16:24:31 +0500

    Creating http get endpoints

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    })
+})
+
+app.get('/health', () => {
+    res.status(200).json({status: 'ok'})
+})
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5000}`)
+})
+
